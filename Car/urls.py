342da59f30f3b97299df003e308701be4b645a68
VN-Bela <bela.vnurture@gmail.com>
2021-08-03 15:44:51 +0530

    Use List View and display Content

--- Car/urls.py
+++ Car/urls.py
@@ -4,6 +4,9 @@ from .import  views
 app_name='Car'
 
 urlpatterns = [
-    path('',views.index),
-    path('parts_data',views.CarListView.as_view(),name='parts_data'),
+    path('',views.CarListView.as_view(),name="Data"),
+    # path('', views.index ,name="Data"),
+    path('parts_data',views.CarCreateView.as_view(),name='parts_data'),
+    path("car_detail/<str:pk>",views.CarDetailIndexView,name="car_detail"),
+   
 ]
--- Car/views.py
+++ Car/views.py
@@ -8,20 +8,36 @@ from .forms import Car_Part_Form
 # Create your views here.
 # function based View
 def index(request):
-    #return HttpResponse("Welcome to Car Part Selling ")
-    return render(request,'Car/index.html')
+    # return HttpResponse("Welcome to Car Part Selling ")
+   return render(request,'Car/index.html')
 
 # class based View
 class CarListView(ListView):
-    model=Car
-    template_name='Car/CarHome.html'
-    def get_queryset(self):
-        context = Car.objects.all()
-        return context
+    # model=Car
+    queryset = Car.objects.all()
+    template_name='Car/index.html'
+    # def get_queryset(self, request):
+    #     context = Car.objects.all()
+    #     return context
 
 class CarCreateView(CreateView):
     model=Car
-    fields=['Car_name']
+    template_name='Car/CarHome.html'
+    fields=[
+            'Car_name',
+            'Car_model',
+            'Car_Part_Name',
+            'Car_Part_Info',
+            'Car_Part_Cat',
+            'Car_Part_Discription',
+            'Owner_info',
+        ]
+    def form_valid(self,form):
+        form.save()
+    
+class CarDetailIndexView(ListView):
+    model=Car
+    template_name="Car/Car_detail.html"
 
     
 
